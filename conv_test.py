e94fda2c9a26cb4a1d9072bfb978e8bab5ac6701
Ned Ruggeri <ruggeri@self-loop.com>
2017-03-22 19:37:38 -0700

    convolutional network: simplify 45deg discrete derivative test

--- conv_test.py
+++ conv_test.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import math
 from net import Net
 import numpy as np
 from functions import deconvolve2d
@@ -8,67 +9,39 @@ from simple_trainer import SimpleTrainer
 im = Image.open("./imgs/convolve_test3.jpeg")
 data = np.array(im)
 data = np.transpose(data, (2,0,1))
+data = data[0:1, :, :]
 
 # create net
 net = Net()
-net.add_rank3_input_layer((1, 4, 4))
+net.add_rank3_input_layer(data.shape)
 net.add_conv_layer(3, 3, 1)
 net.add_output_layer()
 
 # get observed output
-# net.layers[1].weights = np.array([
-#     [
-#         np.zeros([3, 3]),
-#         [
-#         [-1,0,1],
-#         [-2,0,2],
-#         [-1,0,1]
-#         ],
-#         np.zeros([3, 3])
-#     ]
-# ])
-#
-# data = data.astype('float64')
-# data /= data.max()
-# observed = np.copy(net.forward_propagate(data))
-# observed /= observed.max()
+net.layers[1].weights[0, 0] = np.array([
+    [-10, 0, 0],
+    [0, 0, 0],
+    [0, 0, 10]
+])
+
+data = data.astype('float64')
+observed = np.copy(net.forward_propagate(data))
 
 # randomize weights
-# net.layers[1].weights = net.layers[1].initialize_weights()
+net.layers[1].weights = net.layers[1].initialize_weights()
 
-# train
-data = np.ones([1, 4, 4]) * 4
-observed = np.ones([1, 4, 4]) * 2
-
-net.layers[1].weights.fill(1)
-print(net.layers[1].weights)
-t = SimpleTrainer(net, 0.0001)
-for i in range(1000):
+t = SimpleTrainer(net, 1.0)
+prev_cost = 10 ** 10
+for i in range(10000):
     t.train_with_examples([(data, observed)])
-t.learning_rate *= 0.1
-for i in range(9000):
-    t.train_with_examples([(data, observed)])
+    out = net.forward_propagate(data)
+    cost = math.sqrt(((observed - out) ** 2).sum())
+    if cost > prev_cost:
+        print("COST INCREASED")
+        t.learning_rate *= 0.1
+    prev_cost = cost
+
 
 print(net.layers[1].weights)
-print(net.layers[1].weights.sum())
 print(net.forward_propagate(data))
-
-# print(data.shape)
-# print(convlayer.output.shape)
-# convlayer.forward_propagate()
-# print(convlayer.output)
-# out = np.transpose(convlayer.output, (1,2,0))
-# # out = np.absolute(out)
-# # out = out / out.max()
-# # out = out * 255
-# out = out.astype(np.uint8)
-#
-# img = Image.fromarray(out, 'RGB')
-# img.show()
-#
-#
-# from PIL import ImageFile
-#
-# ImageFile.MAXBLOCK = 2**20
-
-# img.save("relu.jpg", "JPEG", quality=80, optimize=True, progressive=True)
+print(cost)
