d6f27c9876646218f271fbb9ff57fc7ccd01b54d
Ned Ruggeri <ruggeri@self-loop.com>
2017-04-05 10:41:40 -0700

    start trying to support a 2d kernel

--- ned_convolution_test_avx.c
+++ ned_convolution_test_avx.c
@@ -8,16 +8,15 @@ void convolve1d(float* input,
                 float* kernel,
                 float* destination,
                 shape_t image_shape,
-                shape_t kernel_shape) {
-
-  assert(kernel_shape.height == 1);
-  assert(kernel_shape.width == 3);
+                int kernel_width,
+                int kernel_row_offset) {
+  assert(kernel_width == 3);
 
   assert(image_shape.width % 8 == 0);
 
-  float k0_arr[8] = { [0 ... 7] = mat_get(kernel, kernel_shape, 0, 0) };
-  float k1_arr[8] = { [0 ... 7] = mat_get(kernel, kernel_shape, 0, 1) };
-  float k2_arr[8] = { [0 ... 7] = mat_get(kernel, kernel_shape, 0, 2) };
+  float k0_arr[8] = { [0 ... 7] = kernel[0] };
+  float k1_arr[8] = { [0 ... 7] = kernel[1] };
+  float k2_arr[8] = { [0 ... 7] = kernel[2] };
   __m256 k0_avx = _mm256_loadu_ps(k0_arr);
   __m256 k1_avx = _mm256_loadu_ps(k1_arr);
   __m256 k2_avx = _mm256_loadu_ps(k2_arr);
@@ -41,6 +40,11 @@ void convolve1d(float* input,
   __m256 prod2_avx;
 
   for (int i = 0; i < image_shape.height; i++) {
+    int destination_row_idx = i + kernel_row_offset;
+    if (!row_in_bounds(image_shape, destination_row_idx)) {
+      continue;
+    }
+
     for (int j = 0; j < image_shape.width; j += 8) {
       data_avx = _mm256_loadu_ps(mat_offset(input, image_shape, i, j));
       prod0_avx = _mm256_mul_ps(k0_avx, data_avx);
@@ -52,12 +56,36 @@ void convolve1d(float* input,
       prod2_avx = _mm256_permutevar8x32_ps(prod2_avx, left_shift_avx);
       prod2_avx = _mm256_and_ps(prod2_avx, drop_right_el_avx);
 
-      result_avx = _mm256_add_ps(prod0_avx, prod1_avx);
+      // TODO: need to add left and right ends of the 8float
+      // array... Argh!
+
+      float* destination_addr = \
+        mat_offset(destination, image_shape, destination_row_idx, j);
+      result_avx = _mm256_loadu_ps(destination_addr);
+
+      result_avx = _mm256_add_ps(result_avx, prod0_avx);
+      result_avx = _mm256_add_ps(result_avx, prod1_avx);
       result_avx = _mm256_add_ps(result_avx, prod2_avx);
 
-      float* destination_offset = mat_offset(destination, image_shape, i, j);
-      _mm256_storeu_ps(destination_offset, result_avx);
+      _mm256_storeu_ps(destination_addr, result_avx);
     }
+  }
+}
+
+void convolve2d(float* input,
+                float* kernel,
+                float* destination,
+                shape_t image_shape,
+                shape_t kernel_shape) {
+  int mid_i = kernel_shape.height / 2;
+  for (int i = 0; i < kernel_shape.height; i++) {
+    int kernel_row_offset = i - mid_i;
+    convolve1d(input,
+               kernel,
+               destination,
+               image_shape,
+               kernel_shape.width,
+               kernel_row_offset);
   }
 }
 
@@ -76,7 +104,11 @@ int main() {
 
   long t = clock();
   for (size_t i = 0; i < ITERS; i++) {
-    convolve1d(input, kernel, destination, image_shape, kernel_shape);
+    convolve2d(input,
+               kernel,
+               destination,
+               image_shape,
+               kernel_shape);
   }
   t = clock() - t;
 
