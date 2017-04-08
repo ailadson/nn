b7ffc304061629833e51111b55fc6600d1f404bd
Anthony Ladson <aladsonjr@gmail.com>
2017-04-08 15:09:41 -0700

    refactor input layer

--- layers/input_layer.py
+++ layers/input_layer.py
@@ -5,7 +5,8 @@ class InputLayer:
 
     def set_input(self, input_v):
         if len(input_v) != self.num_units:
-            raise Exception(f"Wrong number of units in input: {len(input_v)} | Expected: {self.num_units}")
+            raise Exception(f"Wrong number of units in input: {len(input_v)}"
+                            f" | Expected: {self.num_units}")
         self.output = input_v
 
     def forward_propagate(self):
