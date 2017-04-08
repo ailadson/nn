522f851f2da29c971b59a48be2a3204a9373b1a8
Anthony Ladson <aladsonjr@gmail.com>
2017-04-08 15:51:08 -0700

    cleanup tests

--- avx_test.py
+++
@@ -1,8 +0,0 @@
-import pyx.avx_convolve2d_py as avx_convolve2d_py
-import time
-
-t = time.time()
-for i in range(1_000_000):
-    val = avx_convolve2d_py.run_test()
-print(val)
-print(time.time() - t)
