60649ea06d5e87dfe491684ab34bd82eff93dca6
Anthony Ladson <aladsonjr@gmail.com>
2017-04-08 16:42:09 -0700

    refactor trainer

--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+import config
 from functions import *
 import numpy as np
 
@@ -15,7 +16,9 @@ class Trainer:
             if not layer.has_weights():
                 weights_mats.append(None)
             else:
-                weights_mats.append(np.zeros(layer.weights.shape, dtype=np.float32))
+                weights_mats.append(
+                    np.zeros(layer.weights.shape, dtype=config.FLOAT_TYPE)
+                )
         return weights_mats
 
     def init_bias_deriv_vecs(self):
@@ -24,60 +27,61 @@ class Trainer:
             if not layer.has_weights():
                 bias_vecs.append(None)
             else:
-                bias_vecs.append(np.zeros(layer.biases.shape, dtype=np.float32))
+                bias_vecs.append(
+                    np.zeros(layer.biases.shape, dtype=config.FLOAT_TYPE)
+                )
         return bias_vecs
 
-    def train_with_examples(self,examples):
+    def train_with_examples(self, examples):
+        self.reset()
+
+        num_examples = len(examples)
         loss_sum = 0
         misclassification = 0
         for example in examples:
             loss, misclas = self.train_with_example(example)
             loss_sum += loss
             misclassification += misclas
-        self.update_weights_and_biases(len(examples))
-        return ( loss_sum/len(examples), misclassification/len(examples) )
+        self.update_weights_and_biases_of_net(num_examples)
+        return (loss_sum / num_examples, misclassification / num_examples)
 
     def train_with_example(self, example):
-        ipt, opt = example
+        ipt, expected_output = example
         output = self.net.forward_propagate(ipt)
-        self.net.back_propagate(opt)
-        self.update_weight_deriv_mats_and_bias_vecs()
-        # loss = cross_entropy(output, list(opt).index(1))
-        loss = 0.0
-        estimated_true_class_idx = list(output).index( max(output) )
-        misclassification = 1 if list(opt).index( 1 ) != estimated_true_class_idx else 0
+        expected_true_class_idx = list(expected_output).index(1)
+
+        self.net.back_propagate(expected_output)
+        self.accumulate_weight_deriv_mats_and_bias_vecs()
+        estimated_true_class_idx = list(output).index(max(output))
+
+        loss = cross_entropy(output, expected_true_class_idx)
+        misclassification = (
+            1 if expected_true_class_idx != estimated_true_class_idx else 0
+        )
         return (loss, misclassification)
 
-    def inner_update_weight_deriv_mats_and_bias_vecs1(self, layer, i):
-        temp_weight_mat = self.weight_deriv_mats[i]
-        temp_weight_mat += layer.deriv_cache.weights
-
-
-    def inner_update_weight_deriv_mats_and_bias_vecs2(self, layer, i):
-        temp_bias_vec = self.bias_deriv_vecs[i]
-        temp_bias_vec += layer.deriv_cache.biases
-
     #accumulate derivative over course of batch
-    def update_weight_deriv_mats_and_bias_vecs(self):
+    def accumulate_weight_deriv_mats_and_bias_vecs(self):
         for i, layer in enumerate(self.net.layers):
             if not layer.has_weights(): continue
-            self.inner_update_weight_deriv_mats_and_bias_vecs1(layer, i)
-            self.inner_update_weight_deriv_mats_and_bias_vecs2(layer, i)
-            # print("self.weights")
-            # print(self.weight_deriv_mats[i].flags.f_contiguous)
-            # print("layer.weights")
-            # print(layer.deriv_cache.weights.flags.f_contiguous)
-            # print("self.biases")
-            # print(self.bias_deriv_vecs[i].flags.f_contiguous)
-            # print("layer.biases")
-            # print(layer.deriv_cache.biases.flags.f_contiguous)
-            # print("-"*88)
+            # Not sure if incrementing a numpy obj inside a list creates a copy
+            # of the numpy array
+            temp_weight_mat = self.weight_deriv_mats[i]
+            temp_weight_mat += layer.deriv_cache.weights
+            temp_bias_vec = self.bias_deriv_vecs[i]
+            temp_bias_vec += layer.deriv_cache.biases
 
     #at the end of batch, perform update
-    def update_weights_and_biases(self, num_of_examples):
+    def update_weights_and_biases_of_net(self, num_of_examples):
         for i, layer in enumerate(self.net.layers):
             if not layer.has_weights(): continue
             self.weight_deriv_mats[i] *= (-self.learning_rate / num_of_examples)
             self.bias_deriv_vecs[i] *= (-self.learning_rate / num_of_examples)
             layer.weights += self.weight_deriv_mats[i]
             layer.biases += self.bias_deriv_vecs[i]
+
+    def reset(self):
+        for i, layer in enumerate(self.net.layers):
+            if layer.has_weights():
+                self.weight_deriv_mats[i] *= 0
+                self.bias_deriv_vecs[i] *= 0
